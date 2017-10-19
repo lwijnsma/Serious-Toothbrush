d82ecea2057768f29dbcfc2db425ea569ecbea3e
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-19 10:43:49 +0200

    updated design

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
@@ -7,8 +6,8 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style> 
-	 
+  <style>
+
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
@@ -17,42 +16,42 @@
     .navbar li a {
 		color:white;
 	}
-	
+
 	.navbar li a:hover {
 		background-color:#555;
 	}
-	
+
     .row.content {height: 450px}
-    
+
     .sidenav {
       padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
-    
+
     footer {
       background-color: #555;
       color: white;
       padding: 15px;
     }
-    
+
 	 .jumbotron {
       margin-bottom: 0;
     }
-	
+
 	@media screen and (max-width: 767px) {
       .sidenav {
         height: auto;
         padding: 15px;
       }
-      .row.content {height:auto;} 
+      .row.content {height:auto;}
     }
   </style>
 </head>
 <body>
 <div class="jumbotron">
   <div class="container text-center">
-    <h1>Serious Toothbrush</h1>      
+    <h1>Serious Toothbrush</h1>
     <p>slogan</p>
   </div>
 </div>
@@ -62,7 +61,7 @@
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
+        <span class="icon-bar"></span>
       </button>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar"\>
@@ -79,24 +78,24 @@
     </div>
   </div>
 </nav>
-  
-<div class="container-fluid text-center">    
+
+<div class="container-fluid text-center">
   <div class="row content">
-  
+
     <div class="col-sm-2 sidenav">
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
       <p><a href="#">Link</a></p>
     </div>
-	
-    <div class="col-sm-8 text-left"> 
+
+    <div class="col-sm-8 text-left">
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
       <h3>Test</h3>
       <p>Lorem ipsum...</p>
     </div>
-	
+
     <div class="col-sm-2 sidenav">
       <div class="well">
         <p>Top 10</p>
@@ -105,7 +104,7 @@
         <p>Top 10</p>
       </div>
     </div>
-	
+
   </div>
 </div>
 

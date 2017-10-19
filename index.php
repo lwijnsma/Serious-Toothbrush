ce0893918960c48438cbe508bf829de7ec83495e
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-19 19:46:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-  <title>Bootstrap Example</title>
+  <title>serious toothbrush</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">

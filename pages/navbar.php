f8051fb7c9a225b77686d481fcbfcbb699b99c0a
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-20 12:49:59 +0200

    Add files via upload

---
+++ pages/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
+  <div class="container-fluid">
+   
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Store</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Library</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user" aria-hidden="true"></i>  Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			<form class="form-inline">
+				<input type="text" class="form-control" placeholder="Username/Email">			
+				<input type="password" class="form-control" placeholder="Password">
+			</form>
+			
+			<button style="margin:1px;" class="btn btn-sm align-middle btn-outline-success" type="submit">Login</button>
+			<button style="margin:1px;" class="btn btn-sm align-middle btn-outline-secondary">Register</button></br>
+			
+			<a href="#" style="color:grey; margin:5px;">forgot password</a>			
+        </div>
+      </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+         
+        </div>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>

922150d482443dc9ba700091908a6143a5c84358
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-20 11:14:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <style>
 
     .navbar {

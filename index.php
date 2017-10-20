4ea513465cec9b2717b2fb9ea7aa8b3f687f4c2c
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-20 12:24:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="js/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <style>
 
-    .navbar {
+    .navba {
       margin-bottom: 0;
       border-radius: 0;
 	  background-color:#33cccc;
@@ -21,32 +22,43 @@
 	.navbar li a:hover {
 		background-color:#555;
 	}
-
-    .row.content {height: 450px}
-
+	
     .sidenav {
-      padding-top: 20px;
+      padding: 20px;
       background-color: #f1f1f1;
-      height: 100%;
-    }
-
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
+	}
 
 	 .jumbotron {
       margin-bottom: 0;
     }
-
-	@media screen and (max-width: 767px) {
+	
+	html {
+		position: relative;
+		min-height: 100%;
+		}
+		
+	body {
+		margin-bottom: 60px;
+		}
+		
+	.footer {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		height: 60px;
+		line-height: 60px; 
+		background-color: #555;
+		color: white;
+		}
+		
+	@media screen and (max-width: 600px) {
       .sidenav {
         height: auto;
         padding: 15px;
       }
-      .row.content {height:auto;}
+     	 .row.content {height:auto;}
     }
+	
   </style>
 </head>
 <body>
@@ -56,25 +68,45 @@
     <p>slogan</p>
   </div>
 </div>
-<nav class="navbar navbar-inverse>
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar"\>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Store</a></li>
-        <li><a href="#">Library</a></li>
-        <li><a href="#">Contact</a></li>
+   
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Store</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Library</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user" aria-hidden="true"></i>  Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+			<form class="form-inline">
+				<input type="text" class="form-control" placeholder="Username/Email">			
+				<input type="password" class="form-control" placeholder="Password">
+			</form>
+			
+			<button style="margin:1px;" class="btn btn-sm align-middle btn-outline-success" type="submit">Login</button>
+			<button style="margin:1px;" class="btn btn-sm align-middle btn-outline-secondary">Register</button></br>
+			
+			<a href="#" style="color:grey; margin:5px;">forgot password</a>			
+        </div>
+      </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+         
+        </div>
+      </li>
       </ul>
     </div>
   </div>
@@ -98,19 +130,28 @@
     </div>
 
     <div class="col-sm-2 sidenav">
-      <div class="well">
-        <p>Top 10</p>
+	<div class="card">
+      <div class="card-body">
+        <h3 class="card-title">Top 10</h3>
+		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
-      <div class="well">
-        <p>Top 10</p>
+	  </div>
+	  </br>
+	  <div class="card">
+      <div class="card-body">
+        <h3 class="card-title">Top 10</h3>
+		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
+	  </div>
     </div>
 
   </div>
 </div>
 
-<footer class="container-fluid text-center">
+<footer class="footer">
+<div class="container">
   <p>Footer Text</p>
+  </div>
 </footer>
 </body>
 </html>

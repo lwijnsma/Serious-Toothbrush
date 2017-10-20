c47163ed19bd7460733aa62d83926786fad77bb8
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-20 13:42:38 +0200

    Add files via upload

---
+++ pages/cart.php
@@ -0,0 +1,54 @@
+<div class="container" style="margin-top: 20px;">
+	<div class="row">
+		<div class="col">
+			<div class="card card-info">
+				<div class="card-heading">
+					<div class="card-title">
+						<div class="row"  style="margin-top: 20px;">
+							<div class="col" style="margin-left: 20px;">
+								<h5><span class="glyphicon glyphicon-shopping-cart"></span> Shopping Cart</h5>
+							</div>
+							<div class="col" style="margin-right: 20px;">
+								<button type="button" class="btn btn-primary btn-sm btn-block">
+									<span class="glyphicon glyphicon-share-alt"></span> Continue shopping
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col"><img class="img-responsive" src="http://placehold.it/100x70">
+						</div>
+						<div class="col">
+							<h4 class="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+						</div>
+						<div class="col">
+							<div class="col text-right">
+								<h6><strong>00.00</strong></h6>
+							</div>
+							<div class="col">
+								<button type="button" class="btn btn-link btn">
+									<span class="glyphicon glyphicon-trash"> </span>
+								</button>
+							</div>
+						</div>
+					</div>
+					<hr>
+				</div>
+				<div class="card-footer">
+					<div class="row text-center">
+						<div class="col">
+							<h4 class="text-right">Total <strong>00,00</strong></h4>
+						</div>
+						<div class="col">
+							<button type="button" class="btn btn-success btn-block">
+								Checkout
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

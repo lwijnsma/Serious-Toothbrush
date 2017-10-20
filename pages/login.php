504b8e66bac547e802b948639725acff82459e4d
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-20 12:50:12 +0200

    Add files via upload

---
+++ pages/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<head>
+  <title>serious toothbrush</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Login</h2>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Username / Email" required autofocus>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-success" type="submit">Sign in</button>
+		<button class="btn btn-lg btn-primary" type="Button">Register</button>
+      </form>
+
+    </div>
+
+
+<footer class="footer">
+<div class="container">
+  <p>Footer Text</p>
+  </div>
+</footer>
+</body>
+</html>

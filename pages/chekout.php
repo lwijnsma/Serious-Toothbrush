9e9072fbccd966237d1e824e9a4abd3519931e2f
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 12:19:52 +0200

    Add files via upload

---
+++ pages/chekout.php
@@ -0,0 +1,135 @@
+
+<div class="container" style="margin-top: 20px;">
+	<h3>Products</h3>
+	<div class="row">
+		<div class="col">
+			<div class="card card-info">	
+				<div class="card-body">
+						<div class="row">
+						<div class="col"><img class="img-responsive" src="http://placehold.it/100x70">
+						</div>
+						<div class="col">
+							<h4 class="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
+						</div>
+						<div class="col">
+							<div class="col text-right">
+								<h6><strong>25.00</strong></h6>
+							</div>
+							<div class="col text-right">
+								<button type="button" class="btn btn-sm btn-dark">
+									<i class="fa fa-trash fa-2x" aria-hidden="true"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+					<hr>
+				</div>
+				<div class="card-footer">
+					<div class="row text-center">
+						<div class="col">
+							<h4 class="text-right">Total <strong>00,00</strong></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+</br>
+<div class="container">
+<h3>Paymen method</h3>
+      <div class="list-group">
+        <div class="list-group-item">
+          <div class="list-group-item-heading">          
+              <div class="row radio">
+                <div class="col">
+                  <label>
+                    <input type="radio" name="checkout" id="option1" value="iDeal" >
+                    iDeal
+                  </label>
+                </div>
+                    <div class="col"> 
+                     <div class="form-group">
+    				<label for="exampleFormControlSelect1">select bank</label>
+    				<select class="form-control" id="exampleFormControlSelect1">
+      				<option>ING</option>
+     	 			<option>ABN Ambro</option>
+      				<option>Rabobank</option>
+      				<option>Knab</option>
+      				<option>SNS</option>
+    				</select>
+  					</div>
+                    </div>
+                    <div class="col">
+                    <button type="button" class="btn btn-primary" disabled>Checkout</button>
+                    </div>
+                </div>
+              </div>
+          </div>
+           <div class="list-group-item">
+          <div class="list-group-item-heading">          
+              <div class="row radio">
+                <div class="col">
+                  <label>
+                    <input type="radio" name="checkout" id="option2" value="CC" >
+                    Creddit Card
+                  </label>
+                </div>
+                     
+                    <div class="col"> 
+                     <div class="form-group">
+    				<label for="exampleFormControlSelect1">select Credditcard</label>
+    				<select class="form-control" id="exampleFormControlSelect1">
+      				<option>Visa</option>
+     	 			<option>Master Card</option>
+      				<option>Discover</option>
+      				<option>American Expres</option>
+    				</select>
+  					</div>
+                    </div>
+                    <div class="col">
+                    <button type="button" class="btn btn-primary" disabled>Checkout</button>
+                    </div>
+                </div>
+              </div>
+          </div>
+           <div class="list-group-item">
+          <div class="list-group-item-heading">          
+              <div class="row radio">
+                <div class="col">
+                  <label>
+                    <input type="radio" name="checkout" id="option3" value="paypal" >
+                    PayPal
+                  </label>
+                </div>
+                    <div class="col"> 
+                    <input type="text" class="form-control" placeholder="PayPal e-mail" >
+                    </div>
+                    <div class="col">
+                    <button type="button" class="btn btn-primary" disabled>Checkout</button>
+                    </div>
+                </div>
+              </div>
+          </div>
+           <div class="list-group-item">
+          <div class="list-group-item-heading">          
+              <div class="row radio">
+                <div class="col">
+                  <label>
+                    <input type="radio" name="checkout" id="option4" value="voucher" checked>
+                    Voucher
+                  </label>
+                </div>
+                    <div class="col"> 
+                    <input type="text" class="form-control" placeholder="Code" >
+                    </div>
+                    <div class="col">
+                    <button type="button" class="btn btn-primary">Checkout</button>
+                    </div>
+                </div>
+              </div>
+          </div>
+      </br>
+   </div>
+</div>
+         

de4f269643737ac747e8f7bfe7d0383889759537
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-24 01:19:13 +0200

    Add files via upload

--- include/Profile_script.php
+++ include/Profile_script.php
@@ -1,7 +1,30 @@
 <?php
+include 'cfg/connection.php';
 if(isset($_POST['profile_change']))
 {
-  echo "boe";
+$first_name=htmlspecialchars($_POST['profile_name']);
+$first_name=stripslashes ($first_name);
+$first_name=trim($first_name);
+$first_name = mysqli_real_escape_string($db, $first_name);
+
+$last_name=htmlspecialchars($_POST['profile_lastname']);
+$last_name=stripslashes ($last_name);
+$last_name=trim($last_name);
+$last_name = mysqli_real_escape_string($db, $last_name);
+
+$email=htmlspecialchars($_POST['profile_email']);
+$email=stripslashes ($email);
+$email=trim($email);
+$email = mysqli_real_escape_string($db, $email);
+
+$query= "update USERS set first_name='".$first_name."',last_name='".$last_name."',email='".$email."' where id='".$_SESSION["gerbruiker_informatie"]["id"]."'";
+mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+
+$query = 	"SELECT * FROM USERS
+WHERE id ='8'";
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+$result=mysqli_fetch_assoc($result)or die("FOUT : " . mysqli_error());
+$_SESSION["gerbruiker_informatie"]=$result;
 }
 
 

3539d481b91d362d03fe5654330b397c09426416
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 15:22:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include 'pages/home.php';
 <footer class="footer">
 <div class="container">
   <div class="row justify-content-md-center"">
-  <p><?php echo date("Y"); ?> serious toothbrush</p>
+  <p> &copy; <?php echo date("Y"); ?> serious toothbrush</p>
   </div>
   </div>
 </footer>

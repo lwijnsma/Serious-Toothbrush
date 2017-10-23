519b5e4d4dcfbdf7224c7a784f8f5d1f5777b9b5
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 14:19:51 +0200

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,16 +1,25 @@
+ </br>
  <div class="container">
-
+  <div class="row justify-content-md-center"">
+<div class="card">
+  <div class="card-body"
       <form class="form-signin">
         <h2 class="form-signin-heading">Login</h2>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Username / Email" required autofocus>
+        </br>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+      </br>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
+      </br>
+      <div class="row justify-content-md-center"">
+        <div class="col">
         <button class="btn btn-lg btn-success" type="submit">Sign in</button>
+      </div>
+        <div class="col">
 		<button class="btn btn-lg btn-primary" type="Button">Register</button>
+  </div>
+  </div>
       </form>
-
+</div>
+</div>
     </div>
+  </div>

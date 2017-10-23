9ea2c0510fbaf50246502f5242a9de748685fad8
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 16:41:33 +0200

    Add files via upload

---
+++ pages/library.php
@@ -0,0 +1,49 @@
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <h4>User Name</h4>
+      <ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Categories</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Albums</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Songs</a>
+  </li>
+</ul><br>
+    </div>
+
+
+    <div class="col">
+    </br>
+       <div class="card">
+  <div class="card-body">
+     Library
+    </div>
+  </div>
+</br>
+  <div class="card">
+  <div class="card-body">
+     Player
+    </div>
+  </div>
+</div>
+<div class="col-sm-2 sidenav">
+      <h4>Que</h4>
+      <ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Song 1</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Song 2</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Song 3</a>
+  </li>
+</ul><br>
+    </div>
+</div>
+</div>
+    

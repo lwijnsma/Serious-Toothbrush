da976a4f2ed5fb0c8d30ccdd565a263085fa109c
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 11:48:01 +0200

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,25 +1,17 @@
 <div class="container-fluid text-center">
   <div class="row content">
-
-    <div class="col-sm-2 sidenav">
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-      <p><a href="#">Link</a></p>
-    </div>
-
-    <div class="col-sm-8 text-left">
+    <div class="col text-left">
       <h1>Welcome</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <hr>
       <h3>Test</h3>
       <p>Lorem ipsum...</p>
     </div>
-
-    <div class="col-sm-2 sidenav">
+    <div class="col-2 sidenav">
 	<div class="card">
       <div class="card-body">
         <h3 class="card-title">Top 10</h3>
-		<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
 	  </div>
 	  </br>
@@ -30,6 +22,5 @@
       </div>
 	  </div>
     </div>
-
   </div>
 </div>

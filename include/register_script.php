39f1e73142f368b83fec3ddd9b5a006f331345e0
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 14:26:06 +0200

    Update register_script.php

--- include/register_script.php
+++ include/register_script.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); //begin sessie
-include 'connection.php';
+include 'cfg/connection.php';
 
 // controleren of pagina correct is aangeroepen en of er waarden in de velden staan.
 if (isset($_POST['register']))

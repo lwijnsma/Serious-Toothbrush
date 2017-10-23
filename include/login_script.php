671bbf10408a8fb8c783b1ce6d68fb146213e931
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 16:06:58 +0200

    Add files via upload

--- include/login_script.php
+++ include/login_script.php
@@ -2,33 +2,44 @@
 session_start(); // sessie beginnen
 include 'cfg/connection.php';
 // controleren of pagina correct is aangeroepen en of er waarden in de velden staan.
-  if (!empty($_POST)&& $_POST['user']!=""&& $_POST['password']!="")
+  if (!empty($_POST)&& $_POST['user']!="")
   {
     //maakt variable
-    $user=$_POST['user'];
+    $user=htmlspecialchars($_POST['user']);
+    $user=stripslashes ($user);
+    $user=trim($user);
+    $user = mysqli_real_escape_string($db, $user);
     $password=$_POST['password'];
     //hier wordt de hash opgehaalt
-    $query = 	"SELECT password FROM USERS
+    $query =  "SELECT password FROM USERS
     WHERE BINARY username ='" .$user."'";
     $password_result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-    $password_result=mysqli_fetch_assoc($password_result)or die("FOUT : " . mysqli_error());
-
-
-    //hier wordt gekeken of het wachtwoord klopt
-    $password = password_verify($password,$password_result['password']);
-     if ($password==1)
-     {
-
-       echo '<div class="alert alert-success" role="alert">succesful ingelogd</div>';
-       $_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
-       $_SESSION["timeout"]=time() + 120;
-       $_SESSION["gebruiker"]=$user;
-    }
-    else
+    if (mysqli_num_rows($password_result)==1)
     {
-        print '<div class="alert alert-danger" role="alert"> gebruikersnaam of wachtwoord niet correct </div>';
+      $password_result=mysqli_fetch_assoc($password_result)or die("FOUT : " . mysqli_error());
+      //hier wordt gekeken of het wachtwoord klopt
+      $password = password_verify($password,$password_result['password']);
+       if ($password==1)
+       {
+         $query_roll =  "SELECT is_admin FROM USERS
+         WHERE BINARY username ='" .$user."'";
+         $roll_result = mysqli_query($db, $query_roll) or die("FOUT : " . mysqli_error());
+         $roll_result=mysqli_fetch_assoc($roll_result)or die("FOUT : " . mysqli_error());
+         echo '<div class="alert alert-success" role="alert"> je bent ingelogd </div>';
+         $_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
+         $_SESSION["timeout"]=time() + 120;
+         $_SESSION["gebruiker"]=$user;
+         $_SESSION["rol"]=$roll_result["is_admin"];
+       }
+       else
+       {
+         echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
+          die();
+     }
+   }
+     else {
+       echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
        die();
-
-    }
+     }
   }
 ?>
--- include/register_script.php
+++ include/register_script.php
@@ -1,85 +1,76 @@
 <?php
-session_start(); //begin sessie
 include 'cfg/connection.php';
-
 // controleren of pagina correct is aangeroepen en of er waarden in de velden staan.
 if (isset($_POST['register']))
 {
-
   if (!empty($_POST)&& $_POST['user']!=""&& $_POST['password']!=""&& $_POST['repassword']!=""&& $_POST['name']!="" )
   {
-
-     if ($_POST['password']==$_POST['repassword'])
-     {
-
-       $user=htmlspecialchars($_POST['user']);
-       $user=stripslashes ($user);
-       $user=trim($user);
-       $user = mysqli_real_escape_string($db, $user);
-
-       $email=htmlspecialchars($_POST['email']);
-       $email=stripslashes ($email);
-       $email=trim($email);
-       $email = mysqli_real_escape_string($db, $email);
-
-       $name=htmlspecialchars($_POST['name']);
-       $name=stripslashes ($name);
-       $name=trim($name);
-       $name = mysqli_real_escape_string($db, $name);
-
-       $last_name=htmlspecialchars($_POST['lastname']);
-       $last_name=stripslashes ($last_name);
-       $last_name=trim($last_name);
-       $last_name = mysqli_real_escape_string($db, $last_name);
-
-       $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
-       $password=htmlspecialchars($password);
-       $password=stripslashes ($password);
-       $password=trim($password);
-       $password = mysqli_real_escape_string($db, $password);
-
-       $IS_ADMIN = mysqli_real_escape_string($db, 0);
-
-
-       $query = 	"SELECT * FROM USERS
-       WHERE BINARY username ='" . $user ."'";
-         //dit geeft aan of username bestaat
-       $unique = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-       if (strlen($password) >6 )
-       {
-
-           if (mysqli_num_rows($unique) == 0)
+           $user=htmlspecialchars($_POST['user']);
+           $user=stripslashes ($user);
+           $user=trim($user);
+           $user = mysqli_real_escape_string($db, $user);
+           $email=htmlspecialchars($_POST['email']);
+           $email=stripslashes ($email);
+           $email=trim($email);
+           $email = mysqli_real_escape_string($db, $email);
+           $name=htmlspecialchars($_POST['name']);
+           $name=stripslashes ($name);
+           $name=trim($name);
+           $name = mysqli_real_escape_string($db, $name);
+           $last_name=htmlspecialchars($_POST['lastname']);
+           $last_name=stripslashes ($last_name);
+           $last_name=trim($last_name);
+           $last_name = mysqli_real_escape_string($db, $last_name);
+           $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+           $password=htmlspecialchars($password);
+           $password=stripslashes ($password);
+           $password=trim($password);
+           $password = mysqli_real_escape_string($db, $password);
+           $IS_ADMIN = mysqli_real_escape_string($db, 0);
+           if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
            {
-
-              $query1 = 	"INSERT INTO USERS(USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PASSWORD,IS_ADMIN,CREATED_AT,UPDATED_AT)
-              VALUES('".$user."','".$email."','".$name."','".$last_name."','".$password."','".$IS_ADMIN."','".date('Y-m-d')."','".date('Y-m-d')."')";
-              mysqli_query($db, $query1) or die("FOUT : " . mysqli_error());
-              $_POST=array();
-              echo "uw bent geregistreed";
-              sleep(1);
-              header("location:pages/login.php");
-           }
-           else
+            if ($_POST['password']==$_POST['repassword'])
+              {
+           if (strlen($password) >6 )
            {
-              print"Deze gebruikersnaam Bestaat al.";
-           }
+             $query =   "SELECT * FROM USERS
+             WHERE BINARY username ='" . $user ."'";
+             //dit geeft aan of username bestaat
+             $unique = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+               if (mysqli_num_rows($unique) == 0)
+               {
+                  $query1 =   "INSERT INTO USERS(USERNAME,EMAIL,FIRST_NAME,LAST_NAME,PASSWORD,IS_ADMIN,CREATED_AT,UPDATED_AT)
+                  VALUES('".$user."','".$email."','".$name."','".$last_name."','".$password."','".$IS_ADMIN."','".date('Y-m-d')."','".date('Y-m-d')."')";
+                  mysqli_query($db, $query1) or die("FOUT : " . mysqli_error());
+                  $_POST=array();
+                  echo '<div class="alert alert-success" role="alert">uw bent geregistreed</div>';
+                  header("location:pages/login.php");
+               }
+          }
+        }
       }
-      else
-      {
-        print "wachtwoord te kort";
-      }
-    }
-    else
+        $query =  "SELECT * FROM USERS
+        WHERE BINARY username ='" . $user ."'";
+        //dit geeft aan of username bestaat
+        $unique = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+          if (mysqli_num_rows($unique) != 0)
+        {
+           echo '<div class="alert alert-danger" role="alert">This is a danger alert—check it out!</div>';
+        }
+    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
     {
-          print "<p>wachtwoorden zijn niet gelijk</p>";
-       }
-  }
-
+      echo '<div class="alert alert-danger" role="alert">email is niet correct</div>';
+    }
+    if ($_POST['password']!=$_POST['repassword'])
+    {
+      print '<div class="alert alert-danger" role="alert">wachtwoorden zijn niet gelijk</div>';
+    }
+    if (strlen($_POST['password']) <6 )
+    {
+      print '<div class="alert alert-danger" role="alert">wachtwoord te kort</div>';
+    }
+}
   else
-  {
-     print "<p>Alle velden moeten worden ingevuld</p>";
-
-  }
-
+     print '<div class="alert alert-danger" role="alert"> Alle velden moeten worden ingevuld</div>';
 }
  ?>

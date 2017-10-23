0d6877598726d3c4c72d05747064a4e05ac40b34
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-23 15:15:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>serious toothbrush</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/ico" href="images/icono-2016.ico">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="js/jquery.min.js"></script>
   <link rel="stylesheet" href="css/font-awesome.min.css">

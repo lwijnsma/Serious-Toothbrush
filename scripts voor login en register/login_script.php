85354876bf171bfa52727320589c487e17dad6b6
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-23 16:10:55 +0200

    Add files via upload

--- scripts voor login en register/login_script.php
+++ scripts voor login en register/login_script.php
@@ -2,46 +2,57 @@
 session_start(); // sessie beginnen
 include 'connection.php';
 // controleren of pagina correct is aangeroepen en of er waarden in de velden staan.
-  if (!empty($_POST)&& $_POST['user']!=""&& $_POST['password']!="")
+  if (!empty($_POST)&& $_POST['user']!="")
   {
-         $user = mysqli_real_escape_string($db, $_POST['user']);
-         $password = mysqli_real_escape_string($db, $_POST['password']);
-     //sql query//
-     $query = 	"SELECT * FROM USERS
-     WHERE username ='" . $user ."'
-     AND password='" . $password ."''";
+    //maakt variable
+    $user=htmlspecialchars($_POST['user']);
+    $user=stripslashes ($user);
+    $user=trim($user);
+    $user = mysqli_real_escape_string($db, $user);
+
+    $password=$_POST['password'];
+    //hier wordt de hash opgehaalt
+    $query = 	"SELECT password, is_admin FROM USERS
+    WHERE BINARY username ='" .$user."'";
+    $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
 
 
-     $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-
-     if (mysqli_num_rows($result) > 0)
-     {
-          // gebruikersnaam gevonden, registreer gegevens in session
-       $_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
-       $_SESSION["timeout"]=time() + 120;
-       $_SESSION["gebruiker"]=$gebruiker;
-       while($row = mysqli_fetch_assoc($result))
-       {
-          $roll = $row['is_admin'];
-       }
-       // Doorsturen naar beveiligde pagina
-       if(($roll) == "0")
-       {
-          header("Location: user_home.php");
-          exit();
-       }
-       elseif(($rol =="1"))
-       {
-          header("Location: admin.php");
-          exit();
-       }
-
-    }
-    else
+    if (mysqli_num_rows($result)==1)
     {
-        print "gebruikersnaam of wachtwoord niet correct";
-       die();
+      $result=mysqli_fetch_assoc($result)or die("FOUT : " . mysqli_error());
 
-    }
+
+      //hier wordt gekeken of het wachtwoord klopt
+      $password = password_verify($password,$result['password']);
+       if ($password==1)
+       {
+
+         echo "je bent ingelogd";
+
+         ////////////////////////// voer hier het pad in/////////////////
+          header('location: index.php');
+/////////////////////////////////////////////////////////////////////////
+         $_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
+         $_SESSION["timeout"]=time() + 120;
+         $_SESSION["gebruiker"]=$user;
+         $_SESSION["rol"]=$result["is_admin"];
+
+
+
+       }
+
+       else
+       {
+         echo 'Gebruikersnaam of wachtwoord niet correct';
+          die();
+     }
+   }
+     else {
+       echo 'Gebruikersnaam of wachtwoord niet correct';
+       die();
+     }
+
+
   }
+
 ?>

f9bc82fbe1c5b451900f3c27769a74667676bc90
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-24 11:57:33 +0200

    Add files via upload

--- include/navbar.php
+++ include/navbar.php
@@ -1,21 +1,20 @@
 <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
-   
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 	</button>
 
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Store</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Library</a></li>
-      </ul>
-      <ul class="nav navbar-nav ml-auto">
+      <form class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="index.php?page=home">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php?page=store">Store</a></li>
+        <li class="nav-item"><a class="nav-link" href="index.php?page=library">Library</a></li>
+      </form>
+      <form class="nav navbar-nav ml-auto">
         <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-user" aria-hidden="true"></i>  Account</a></li>
-       <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</a> </li>
-      </ul>
+        <a class="nav-link" href="index.php?page=account"><i class="fa fa-user" aria-hidden="true"></i>  Account</a></li>
+       <li class="nav-item"><a class="nav-link" href="index.php?page=cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</a> </li>
+      </form>
     </div>
   </div>
 </nav>

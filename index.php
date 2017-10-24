f1faf41a0dc67f6312fa7768f2e7f3a0bf8aec51
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-24 12:02:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ session_regenerate_id();
 include 'include/header.php';
 ?>
 <?php
-include 'include/navbarbuttons.php';
+include 'include/navbar.php';
 ?>
 <?php
   include 'include/nav_switch.php';

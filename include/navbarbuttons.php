14d78662223df39057a4c5ead5569f9c0482003b
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-24 14:28:08 +0200

    .

--- include/navbarbuttons.php
+++ include/navbarbuttons.php
@@ -7,18 +7,14 @@
 
 <div class="collapse navbar-collapse" id="navbarNav">
 <form class="navbar-nav mr-auto" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" style="height: 30px;">
-  <button type="submit" class="nav-item" name="page" value="Home">Home</button>
-  <button type="submit" class="nav-item" name="page" value="Store">Store</button>
-  <button type="submit" class="nav-item" name="page" value="Library">Library</button>
-</form>
+  <li class="nav-link"><button type="submit" class="nav-item" name="page" value="Home">Home</button></li>
+  <li class="nav-link"><button type="submit" class="nav-item" name="page" value="Store">Store</button></li>
+  <li class="nav-link"><button type="submit" class="nav-item" name="page" value="Library">Library</button></li>
 
 
       <form class="nav navbar-nav ml-auto" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-
-        <button  type="submit" class="nav-item" name="page" value="Account"><i class="fa fa-user" aria-hidden="true"></i> Account</button>
-
-        <button type="submit" class="nav-item" name="page" value="Cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</button>
-
+       <li class="nav-link"><button  type="submit" class="nav-item" name="page" value="Account"><i class="fa fa-user" aria-hidden="true"></i> Account</button></li>
+       <li class="nav-link"> <button type="submit" class="nav-item" name="page" value="Cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</button></li>
       </form>
 
     </div>

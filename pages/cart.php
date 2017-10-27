048c4dc01020053fa76b8778c41a07093c692814
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-27 14:54:57 +0200

    Add files via upload

--- pages/cart.php
+++ pages/cart.php
@@ -11,37 +11,27 @@
 								<h5><i class="fa fa-shopping-cart" aria-hidden="true"></i> Shopping Cart</h5>
 							</div>
 							<div class="col" style="margin-right: 20px;">
-								<button type="button" class="btn btn-primary btn-sm btn-block">
+								<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+								<button type="submit" name="page" class="btn btn-primary btn-sm btn-block" value="Store">
 									<i class="fa fa-share" aria-hidden="true"></i> Continue shopping
 								</button>
+								</form>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="card-body">
-						<div class="row">
-						<div class="col"><img class="img-responsive" src="http://placehold.it/100x70">
-						</div>
-						<div class="col">
-							<h4 class="product-name"><strong>Product name</strong></h4><h4><small>Product description</small></h4>
-						</div>
-						<div class="col">
-							<div class="col text-right">
-								<h6><strong>25.00</strong></h6>
-							</div>
-							<div class="col text-right">
-								<button type="button" class="btn btn-sm btn-dark">
-									<i class="fa fa-trash fa-2x" aria-hidden="true"></i>
-								</button>
-							</div>
-						</div>
-					</div>
+
+
+
+<?php include 'include/cart_items.php'; ?>
 					<hr>
 				</div>
 				<div class="card-footer">
 					<div class="row text-center">
 						<div class="col">
-							<h4 class="text-right">Total <strong>00,00</strong></h4>
+
+							<h4 class="text-right">Total € <strong><?php echo number_format($total,2) ?></strong></h4>
 						</div>
 						<div class="col">
 							<button type="button" class="btn btn-success btn-block">

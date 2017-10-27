1a6e6a4494660ae13666abcee617116a760a78b0
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-27 16:56:41 +0200

    lib

---
+++ include/library_table_input.php
@@ -0,0 +1,44 @@
+<?php include 'cfg/connection.php';
+
+if(!empty($_POST['library_search']))
+{
+
+$search=htmlspecialchars($_POST['library_search']);
+$search=stripslashes ($search);
+$search=trim($search);
+$search = mysqli_real_escape_string($db, $search);
+
+$query="SELECT  title, artiest, album_title  FROM `songs` WHERE title LIKE '%".$search."%' or album_title LIKE '%".$search."%' or artiest LIKE '%".$search."%'
+ORDER BY title";
+ $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+
+ if ($result = $db->query($query)) {
+
+     /* fetch associative array */
+     while ($row = $result->fetch_assoc()) {
+          echo  '<td class="body-item mbr-fonts-style display-7">'.$row['title'] .'</td><td class="body-item mbr-fonts-style display-7">'. $row['artiest']  .'</td><td class="body-item mbr-fonts-style display-7">'. $row['album_title'] .'</td><td class="body-item mbr-fonts-style display-7"><button class="btn btn-sm btn-dark" type="submit" name="add"><i class="fa fa-play" aria-hidden="true"></i></button></td></tr><tr>';
+     }
+
+     /* free result set */
+     $result->free();
+}
+}
+else
+{
+
+  $query="SELECT * FROM `songs` WHERE title LIKE '%%'
+  ORDER BY title";
+   $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+
+   if ($result = $db->query($query)) {
+
+       /* fetch associative array */
+       while ($row = $result->fetch_assoc()) {
+            echo  '<td class="body-item mbr-fonts-style display-7">'.$row['title'] .'</td><td class="body-item mbr-fonts-style display-7">'. $row['artiest']  .'</td><td class="body-item mbr-fonts-style display-7">'. $row['album_title'] .'</td><td class="body-item mbr-fonts-style display-7"><button class="btn btn-sm btn-dark" type="submit" name="add"><i class="fa fa-play" aria-hidden="true"></i></button></td></tr><tr>';
+       }
+
+       /* free result set */
+       $result->free();
+}
+}
+ ?>
--- pages/library.php
+++ pages/library.php
@@ -14,15 +14,42 @@
   </li>
 </ul><br>
     </div>
-
-
     <div class="col">
-    </br>
+          </br>
+    <h4 class="mbr-section-title mbr-fonts-style align-center display-4">Library</h4>
        <div class="card">
   <div class="card-body">
-     Library
+     <div class="container container-table">
+      <div class="table-wrapper">
+        <div class="container">
+          <div class="row search">
+            <div class="col-md-6"></div>
+        </div>
+        <div class="container scroll">
+     <nav aria-label="Page navigation ">
+      <form class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <input class="form-control mr-sm-2" type="search" name="library_search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+  </form>
+          <table class="table" cellspacing="0">
+            <thead>
+              <tr class="table-heads ">
+              <th class="head-item mbr-fonts-style display-7">
+                      NAME</th><th class="head-item mbr-fonts-style display-7">
+                      Artist</th><th class="head-item mbr-fonts-style display-7">
+                      Album</th><th class="head-item mbr-fonts-style display-7">
+                      </th></tr>
+            </thead>
+            <tbody>
+            <?php include("include/library_table_input.php"); ?>
+          </table>
+        </div>
+    </div>
     </div>
   </div>
+</div>
+</div>
 </br>
   <div class="card">
   <div class="card-body">

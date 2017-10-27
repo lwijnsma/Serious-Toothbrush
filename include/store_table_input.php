95287bd68dda8c326b68c3b866a99bfe3882194b
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-27 21:28:00 +0200

    Add files via upload

--- include/store_table_input.php
+++ include/store_table_input.php
@@ -1,6 +1,61 @@
 <?php include 'cfg/connection.php';
 
 
+if(!empty($_POST['store_add']))
+
+{
+if($_SESSION['auth']==true){
+$query="SELECT * FROM library_songs where songs_title ='".$_POST['store_add']."'and libraries_users_id = ".$_SESSION['gerbruiker_informatie']['id']."";
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+
+if(mysqli_num_rows($result)==0)
+
+{
+$query="SELECT * from cart_songs where songs_title='".$_POST['store_add']."'and cart_id =".$_SESSION['gerbruiker_informatie']['id']."";
+var_dump($_POST['store_add']);
+var_dump($_SESSION['gerbruiker_informatie']['id']);
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+if(mysqli_num_rows($result)==0)
+{
+$query="INSERT into cart_songs values('".$_POST['store_add']."',".$_SESSION['gerbruiker_informatie']['id'].")";
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+echo 'song aa shoppingcart toegevoegd';
+}
+else{echo'dit nummer zit al in je shoppincart';}
+}
+else{echo'dit nummer zit al in je library';}
+}
+else{
+  $_POST['page']='Login';
+  $_SESSION['pages']='Login';
+  header('location:redirect.php');
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if(!empty($_POST['store_search']))
 {
@@ -49,37 +104,9 @@ else
 
 }
 
-if(!empty($_POST['store_add']))
-
-{
-if ($_SESSION['auth']=false)
-{
-  $_POST['page']='Login';
-  $_SESSION['pages']='Login';
-  header('location:redirect.php');
-}
-  else{
-$query="SELECT * FROM library_songs where songs_title ='".$_POST['store_add']."'and libraries_users_id = ".$_SESSION['gerbruiker_informatie']['id']."";
-$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-
-if(mysqli_num_rows($result)==0)
-
-{
-$query="SELECT * from cart_songs where songs_title='".$_POST['store_add']."'and cart_id =".$_SESSION['gerbruiker_informatie']['id']."";
-var_dump($_POST['store_add']);
-var_dump($_SESSION['gerbruiker_informatie']['id']);
-$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-if(mysqli_num_rows($result)==0)
-{
-$query="INSERT into cart_songs values('".$_POST['store_add']."',".$_SESSION['gerbruiker_informatie']['id'].")";
-$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-echo 'song aa shoppingcart toegevoegd';
-}
-else{echo'dit nummer zit al in je shoppincart';}
-}
-else{echo'dit nummer zit al in je library';}
-}
-}
 
 }
+
+
+
  ?>

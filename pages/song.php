db615534fca515ac646dfcbf436d368872aa546c
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-28 00:36:06 +0200

    Add files via upload

--- pages/song.php
+++ pages/song.php
@@ -1,52 +1,31 @@
 <div class="container-fluid">
   <div class="row content">
 
-    <div class="col-sm-3">
+    <div class="col-sm-auto">
     </div>
 
     <div class="col">
-    <div class="row">
+  <!---  <div class="row">
       <div class="col">
       </br>
     <h4 class="mbr-section-title mbr-fonts-style align-center display-4">Song title&nbsp;<small>album</small></h4>
       </div>
       <div class="col-sm-3">
       </br></br>
-    <button type="button" class="btn btn-primary btn-sm btn-block"><i class="fa fa-share" aria-hidden="true"></i> Continue shopping</button>
+      <form class="form-inline" action="<?php// echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <button type="submit"  name="page" value="Store"class="btn btn-primary btn-sm btn-block"><i class="fa fa-share" aria-hidden="true"></i> Continue shopping</button>
+</form>
       </div>
     </div>
-
-       <div class="card">
-  <div class="card-body">
-     <div class="row">
-        <div class="col">
-          <img src="http://placehold.it/300x300" alt="">
-        </div>
-        <div class="col">
-          <h3>Song Description</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-          <h3>Song Details</h3>
-          <ul>
-            <li>Artist</li>
-            <li>Album</li>
-            <li>Genre</li>
-          </ul>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-sm-6 ml-auto"> 
-        </div>
-        <div class="col-sm-4 mr-auto">
-          <h4>Price <small><b>$price</b></small></h4>
-        </div>
-        <div class="col mr-auto"><button type="button" class="btn btn-dark"><i class="fa fa-cart-plus" aria-hidden="true"></i></button></div>
-        </div>
-  </div>
+-->
+       <div class="card-body">
+      <?php include "include/song_script.php"; ?>
+         </div>
 </div>
 </div>
 
 <div class="col-sm-3">
-      
+
 </div>
 
 </div>

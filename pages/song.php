bf076b84cf64d1b3bc76d8fefe983894f9beec94
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-27 14:03:48 +0200

    .

---
+++ pages/song.php
@@ -0,0 +1,46 @@
+<div class="container-fluid">
+  <div class="row content">
+
+    <div class="col-sm-3">
+    </div>
+
+    <div class="col">
+    <div class="row">
+      <div class="col">
+      </br>
+    <h4 class="mbr-section-title mbr-fonts-style align-center display-4">Song title&nbsp;<small>album</small></h4>
+      </div>
+      <div class="col-sm-2">
+      </br>
+    <button type="button" class="btn btn-primary btn-sm btn-block"><i class="fa fa-share" aria-hidden="true"></i> Continue shopping</button>
+      </div>
+    </div>
+
+       <div class="card">
+  <div class="card-body">
+     <div class="row">
+        <div class="col">
+          <img class="rounded mx-auto" src="http://placehold.it/300x300" alt="">
+        </div>
+        <div class="col">
+          <h3>Song Description</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+          <h3>Song Details</h3>
+          <ul>
+            <li>Artist</li>
+            <li>Album</li>
+            <li>Genre</li>
+          </ul>
+
+        </div>
+      </div>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-3">
+      
+</div>
+
+</div>
+</div>
---
+++ test.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+session_start( );
+session_regenerate_id();
+?>
+<html>
+<head>
+  <title>serious toothbrush</title>
+  <meta charset="utf-8">
+  <link rel="icon" type="image/ico" href="images/icono-2016.ico">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <script src="js/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include 'include/header.php';
+?>
+<?php
+include 'include/navbar.php';
+?>
+<?php
+include 'pages/song.php';
+?>
+<footer class="footer">
+<div class="container">
+  <div class="row justify-content-md-center"">
+  <p> &copy; <?php echo date("Y"); ?> serious toothbrush</p>
+  </div>
+  </div>
+</footer>
+</body>
+</html>

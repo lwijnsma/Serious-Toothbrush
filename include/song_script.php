562fc06ce678cd526b9bb7f98827053a60aacdce
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-10-28 00:35:37 +0200

    Add files via upload

--- include/song_script.php
+++ include/song_script.php
@@ -1,42 +1,61 @@
 
 
-<div class="card-body">
-   <div class="row">
-      <div class="col">
-        <img src="http://placehold.it/300x300" alt="">
-      </div>
-      <div class="col">
-        <h3>Song Description</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-        <h3>Song Details</h3>
-        <ul>
-          <li>Artist</li>
-          <li>Album</li>
-          <li>Genre</li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-6 ml-auto">
-      </div>
-      <div class="col-sm-4 mr-auto">
-        <h4>Price <small><b>$price</b></small></h4>
-      </div>
-      <div class="col mr-auto"><button type="button" class="btn btn-dark"><i class="fa fa-cart-plus" aria-hidden="true"></i></button></div>
-      </div>
-</div>
+
 <?php
+include 'cfg/connection.php';
+$Song=$_SESSION['Song'];
+include 'include/cart_data_functions.php';
+$query="SELECT * FROM SONGS WHERE TITLE = '".$Song."'";
+$result=mysqli_query($db,$query);
+$result=mysqli_fetch_assoc($result);
+
+echo '<div class="row">';
+echo  '<div class="col">';
+echo  '</br>';
+echo'<h5 class="mbr-section-title mbr-fonts-style align-center display-4">'.$result["title"].'&nbsp;<small>'.$result["album_title"].'</small></h5>';
+echo  '</div>';
+echo  '<div class="col-sm-3">';
+echo  '</br> </br>';
+echo  '<form class="form-inline" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post">';
+echo'<button type="submit"  name="page" value="Store"class="btn btn-primary btn-sm btn-block"><i class="fa fa-share" aria-hidden="true"></i> Continue shopping</button>';
+echo'</form>';
+echo  '</div>';
+echo'</div>';
+
+
+echo'<div class="card">';
+
+
+echo '<div class="row">';
+echo '<div class="col">';
+echo       ' <img src="'.$result["picture_location"].'" alt="">';
+echo      '</div>';
+echo      '<div class="col">';
+echo      '<h3>Song Description</h3>';
+echo        '<p>'.$result["description"].'</p>';
+echo        '<h3>Song Details</h3>';
+echo        '<ul>';
+echo          '<li>'.$result["artiest"].'</li>';
+echo          '<li>'.$result["album_title"].'</li>';
+echo          '<li>'.$result["genre_title"].'</li>';
+echo        '</ul>';
+echo      '</div>';
+echo    '</div>';
+echo    '<div class="row">';
+echo      '<div class="col-sm-6 ml-auto">';
+echo      '</div>';
+echo      '<div class="col-sm-4 mr-auto">';
+echo        '<h4>Price <small><b>€ '.number_format($result["price"],2).'</b></small></h4>';
+echo      '</div>';
+echo "<form action="."'".htmlspecialchars($_SERVER["PHP_SELF"])."'"."method='post'>";
+echo      '<div class="col mr-auto"><button type="input" name="store_add" value="'.$result["title"].'" class="btn btn-dark"><i class="fa fa-cart-plus" aria-hidden="true"></i></button></div>';
+echo '</form>';
 
 
 
 
 
 
-
-
-
-
-unset($_SESSION['song']);
 
 
  ?>
--- include/store_table_input0.php
+++ include/store_table_input0.php
@@ -1,9 +1,36 @@
 <?php include 'cfg/connection.php';
 
 
+if(!empty($_POST['store_add']))
 
-include 'include/cart_data_functions.php';
+{
+if($_SESSION['auth']==true){
+$query="SELECT * FROM library_songs where songs_title ='".$_POST['store_add']."'and libraries_users_id = ".$_SESSION['gerbruiker_informatie']['id']."";
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
 
+if(mysqli_num_rows($result)==0)
+
+{
+$query="SELECT * from cart_songs where songs_title='".$_POST['store_add']."'and cart_id =".$_SESSION['gerbruiker_informatie']['id']."";
+var_dump($_POST['store_add']);
+var_dump($_SESSION['gerbruiker_informatie']['id']);
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+if(mysqli_num_rows($result)==0)
+{
+$query="INSERT into cart_songs values('".$_POST['store_add']."',".$_SESSION['gerbruiker_informatie']['id'].")";
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+echo 'song aa shoppingcart toegevoegd';
+}
+else{echo'dit nummer zit al in je shoppincart';}
+}
+else{echo'dit nummer zit al in je library';}
+}
+else{
+  $_POST['page']='Login';
+  $_SESSION['pages']='Login';
+  header('location:redirect.php');
+}
+}
 
 if(!empty($_POST['song_item_button']))
 {
@@ -13,6 +40,26 @@ $_POST['page']='Song';
 $_SESSION['pages']='Song';
 header('location:redirect.php');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if(!empty($_POST['store_search']))

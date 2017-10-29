709f822d56698e9c755430ad076d6bcb1a17a59d
Berwout Kruit <tehber@hotmail.com>
2017-10-29 15:29:04 +0100

    Patched up login script and login page.

--- include/login_script.php
+++ include/login_script.php
@@ -3,73 +3,89 @@
 include 'cfg/connection.php';
 // controleren of pagina correct is aangeroepen en of er waarden in de velden staan.
 
-  if (isset($_POST['user']))
-  {
-    //maakt variable
-    $user=htmlspecialchars($_POST['user']);
-    $user=stripslashes ($user);
-    $user=trim($user);
-    $user = mysqli_real_escape_string($db, $user);
+if (isset($_POST['user']))
+{
+    $user       = $_POST['user'];
+    $password   = $_POST['password'] ?? null;
+    /*
+        Tijd voor magie, het viel me op dat er geen check was voor als er geen password was
+        dus heb ik een labda toegevoegd, in dit geval ?? dit is nieuw sinds php 7.x
+        en geeft iets een waarde stel het heeft geen waarde/bestaat niet.
 
-    $password=$_POST['password'];
-    //hier wordt de hash opgehaalt
-    $query = 	"SELECT * FROM USERS
-    WHERE BINARY username ='" .$user."'";
+        Als er nu geen wachtwoord ingevult woord heeft $password de waarde null.
+    */
+
+    //$user=htmlspecialchars($_POST['user']);
+    //$user=stripslashes ($user);
+    //$user=trim($user);
+    $user = mysqli_real_escape_string($db, $user);
+    /*
+        Mysqli escape string haalt al trailing spaties etc. weg
+        Daarnaast dealt het met speciale characters.
+    */
+
+    /*$query = 	"SELECT * FROM USERS
+    WHERE username ='" .$user."'";*/
+    $query  = "SELECT * FROM `USERS` WHERE `username` = '$user';";
+    /*
+        In PHP als je dubbele quotes gebruikt kun je variabelen toevoegen.
+        Daarom is bad practice om ze voor tekst te gebruiken maar is het bijvoorbeeld
+        perfect voor de query.
+    */
+
     $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
 
 
     if (mysqli_num_rows($result)==1)
     {
-      $result=mysqli_fetch_assoc($result)or die("FOUT : " . mysqli_error());
+        $result = mysqli_fetch_assoc($result) or die("FOUT : " . mysqli_error());
 
 
-      //hier wordt gekeken of het wachtwoord klopt
-      $password = password_verify($password,$result['password']);
-       if ($password==1)
-       {
+        //hier wordt gekeken of het wachtwoord klopt
+        $password = password_verify($password,$result['password']);
+        if ($password == 1)
+        {
+            echo '<div class="alert alert-success" role="alert"> je bent ingelogd </div>';
 
-         echo '<div class="alert alert-success" role="alert"> je bent ingelogd </div>';
-
-
-
-         $_SESSION["auth"]=true; //auth controleert of een klant is ingelogd
-         $_SESSION["timeout"]=time() + 120;
-         $_SESSION["gebruiker"]=$user;
-         $_SESSION["rol"]=$result["is_admin"];
-         $_SESSION["gerbruiker_informatie"]=$result;
+            $_SESSION["auth"]       = true; //auth controleert of een klant is ingelogd
+            $_SESSION["timeout"]    = time() + 120;
+            $_SESSION["gebruiker"]  = $user;
+            $_SESSION["rol"]        = $result["is_admin"];
+            $_SESSION["gerbruiker_informatie"] = $result;
 
 
 
-                  if($_SESSION['pages']=='Cart')
-                  {
-                  $_POST['page']='Cart';
-                  $_SESSION['pages']='Cart';
-                  header('location: redirect.php');
-                  }
-                  elseif($_SESSION['pages']=='Library')
-                  {
-                  $_POST['page']='Library';
-                  $_SESSION['pages']='Library';
-                  header('location: redirect.php');
-                  }
-                  else {
-                    $_POST['page']='Home';
-                    $_SESSION['pages']='Home';
-                    header('location: redirect.php');
-                  }
-       }
+            if ($_SESSION['pages'] == 'Cart')
+            {
+                $_POST['page']      = 'Cart';
+                $_SESSION['pages']  = 'Cart';
+                header('location: redirect.php');
+            }
+            elseif ($_SESSION['pages'] == 'Library')
+            {
+                $_POST['page']      = 'Library';
+                $_SESSION['pages']  = 'Library';
+                header('location: redirect.php');
+            }
+            else
+            {
+                $_POST['page']      = 'Home';
+                $_SESSION['pages']  = 'Home';
+                header('location: redirect.php');
+            }
+        }
+        else
+        {
+            echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
+            die();
+        }
+    }
+    else
+    {
+        echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
+        die();
+    }
 
-       else
-       {
-         echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
-          die();
-     }
-   }
-     else {
-       echo '<div class="alert alert-danger" role="alert">Gebruikersnaam of wachtwoord niet correct </div>';
-       die();
-     }
-
-  }
+}
 
 ?>
--- pages/login.php
+++ pages/login.php
@@ -1,30 +1,33 @@
 
 </br>
 <div class="container">
- <div class="row justify-content-md-center">
-<div class="card">
- <div class="card-body">
-     <form class="form-signin" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-       <h2 class="form-signin-heading">Login</h2>
-       </br>
+    <div class="row justify-content-md-center">
+        <div class="card">
+            <div class="card-body">
+                <form class="form-signin" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+                    <h2 class="form-signin-heading">Login</h2>
+                    </br>
 
-       <input type="text" name="user" value="" size="20" class="form-control" placeholder="Username" />
-     </br>
-       <input type="password" name="password" value="" size="20" class="form-control" placeholder="Password" />
-     </br>
-     <div class="row justify-content-md-center">
-       <div class="col">
-       <input type="submit" name="" class="btn btn-lg btn-success" value="login">
+                    <input type="text" name="user" value="" size="20" class="form-control" placeholder="Username" />
+                    </br>
 
-     </div>
-       <div class="col">
-   <Button type="submit" Class="btn btn-lg btn-primary" value="Register" name="page">Register</button>
- </div>
- </div>
-<br>
-   </form>
-   <?php include 'include/login_script.php'; ?>
+                    <input type="password" name="password" value="" size="20" class="form-control" placeholder="Password" />
+                    </br>
+
+                    <div class="row justify-content-md-center">
+                        <div class="col">
+                            <input type="submit" name="" class="btn btn-lg btn-success" value="login">
+                        </div>
+
+                        <div class="col">
+                            <button type="submit" Class="btn btn-lg btn-primary" value="Register" name="page">Register</button>
+                        </div>
+                    </div>
+                    </br>
+                </form>
+
+                <?php include 'include/login_script.php'; ?>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
-   </div>
- </div>

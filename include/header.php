2ec9258d8c0e0fd52d1d7e86721be4c5f7ce8db0
manonolthof <33227250+manonolthof@users.noreply.github.com>
2017-10-30 16:47:40 +0100

    .

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
 <div classs="jumbotron" style="height:100px;">
   <div class="container text-center">
     <h1>Serious Toothbrush</h1>
-    <p>slogan</p>
+    <p>Let's put a smile on that face</p>
   </div>
 </div>

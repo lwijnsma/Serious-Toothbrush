feba7da4ceae48eee6989731c51fb8b3ed7f72b7
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-30 11:10:06 +0100

    allerts styled

--- include/cart_data_functions.php
+++ include/cart_data_functions.php
@@ -15,12 +15,12 @@ $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
 if(mysqli_num_rows($result)==0)
 {
 $query="INSERT into cart_songs values('".$_POST['store_add']."',".$_SESSION['gerbruiker_informatie']['id'].")";
-$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
-echo 'song aan shoppingcart toegevoegd';
+$result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error($db));
+echo '<div class="alert alert-success" role="alert">song aan shoppingcart toegevoegd</div>';
 }
-else{echo'dit nummer zit al in je shoppincart';}
+else{echo'<br><div class="alert alert-warning" role="alert">dit nummer zit al in je shoppincart</div>';}
 }
-else{echo'dit nummer zit al in je library';}
+else{echo'<br><div class="alert alert-warning" role="alert">dit nummer zit al in je library</div>';}
 }
 else{
   $_POST['page']='Login';

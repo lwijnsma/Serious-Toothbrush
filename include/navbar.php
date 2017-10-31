e67c57b48ea3d86e6b0dad01a5c942232c4c1a0c
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-31 19:59:42 +0100

    .

--- include/navbar.php
+++ include/navbar.php
@@ -1,21 +1,24 @@
 <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
+
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 	</button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php?page=home">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php?page=store">Store</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php?page=library">Library</a></li>
-      </ul>
-      <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item">
-        <a class="nav-link" href="index.php?page=account"><i class="fa fa-user" aria-hidden="true"></i>  Account</a></li>
-       <li class="nav-item"><a class="nav-link" href="index.php?page=cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</a> </li>
-      </ul>
+<div class="collapse navbar-collapse" id="navbarNav">
+	<ul class="navbar-nav mr-auto">
+<form class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" style="height: 30px;">
+   <li class="nav-item"><button type="submit" class="nav-item" name="page" value="Home">Home</button></li>
+   <li class="nav-item"><button type="submit" class="nav-item" name="page" value="Store">Store</button></li>
+   <li class="nav-item"><button type="submit" class="nav-item" name="page" value="Library">Library</button></li>
+</form>
+</ul>
+<ul class="navbar-nav ml-auto">
+<form class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+     <li class="nav-item"><button  type="submit" class="nav-item" name="page" value="Account"><i class="fa fa-user" aria-hidden="true"></i> Account</button></li>
+     <li class="nav-item"><button type="submit" class="nav-item" name="page" value="Cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>  Cart</button></li>
+</form>
+</ul>
     </div>
-
   </div>
 </nav>

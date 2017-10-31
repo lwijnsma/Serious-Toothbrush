79c7fee66036ac5e1b2d2e62e2e8fdec9a1758b3
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-31 21:35:14 +0100

    that's what i call a header

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,9 @@
 <div  class="header" >
   <div class="container text-center">
-    <h1>Serious Toothbrush</h1>
+    <h1 class="display-1">Serious Toothbrush</h1>
     <p>Let's put a smile on that face</p>
   </div>
 </div>
-<br><br>
 <div class="container-fluid text-center">
    <div class="row content">
       <div class="col-sm-2">

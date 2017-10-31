efda72fc63ce1f744455a03ba1aa79276a9e9faa
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-10-31 21:28:03 +0100

    ta daa

--- pages/home.php
+++ pages/home.php
@@ -4,6 +4,7 @@
     <p>Let's put a smile on that face</p>
   </div>
 </div>
+<br><br>
 <div class="container-fluid text-center">
    <div class="row content">
       <div class="col-sm-2">
@@ -27,23 +28,6 @@
          </div>
       </div>
       <div class="col-sm-2 sidenav">
-         <div class="card">
-            <div class="card-body">
-               <h4 class="card-title">Top 10</h4>
-               <ul class="nav flex-column">
-                  <li class="nav-item">
-                     <a class="nav-link dark" href="#">Song 1</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link dark" href="#">Song 2</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link dark" href="#">Song 3</a>
-                  </li>
-               </ul>
-               <br
-            </div>
-         </div>
       </div>
    </div>
 </div>

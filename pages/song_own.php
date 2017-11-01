5deadd4e847b01643097b8baa277761424c2a7b9
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-11-01 21:33:09 +0100

    un used files

--- pages/song_own.php
+++ pages/song_own.php
@@ -5,19 +5,6 @@
     </div>
 
     <div class="col">
-  <!---  <div class="row">
-      <div class="col">
-      </br>
-    <h4 class="mbr-section-title mbr-fonts-style align-center display-4">Song title&nbsp;<small>album</small></h4>
-      </div>
-      <div class="col-sm-3">
-      </br></br>
-      <form class="form-inline" action="<?php// echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-    <button type="submit"  name="page" value="Store"class="btn btn-primary btn-sm btn-block"><i class="fa fa-share" aria-hidden="true"></i> Continue shopping</button>
-</form>
-      </div>
-    </div>
-  -->
   <div class="card-body">
     <?php include "include/song_script_own.php"; ?>
   </div>

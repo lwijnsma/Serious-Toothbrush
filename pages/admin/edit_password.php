a62728cc3e31dfa17968efff83d1efd50d820a39
Karst <karstvangalenlast@gmail.com>
2017-11-02 12:50:26 +0100

    Merge branch 'master' of https://github.com/tweaker33/Serious-Toothbrush

---
+++ pages/admin/edit_password.php
@@ -0,0 +1,31 @@
+ <div class="col">
+ 	<br>
+       <div class="card">
+  <div class="card-body">
+     <form class="" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+                        
+                        <div class="row">
+      <div class="form-group col-md-4">
+      <label for="">Existing Password </label><input type="password" class="form-control" name="password" >
+      </div>
+    </div>
+    <div class="row">
+  <div class="form-group col-md-4">
+       <label for=""> password </label><input type="password" class="form-control" name="npassword" >
+      </div>
+    <div class="form-group col-md-4">
+       <label for="">re password </label><input type="password" class="form-control" name="repassword" >
+      </div>
+    </div>
+ <div class="row">
+ 	<div class="col">
+      <button class="btn btn-success" type="submit" name="password_change" value="password_change">Change</buttton>
+     	</div
+ </div>
+      </form>
+    </div>
+</br>
+    <?php include 'include/password_script.php' ?>
+  </div>
+</div>
+</div>

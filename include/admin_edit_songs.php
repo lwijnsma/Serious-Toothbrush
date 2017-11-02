f2413e967d7f3c57e218f4dce71b888561b412a4
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-11-02 23:29:17 +0100

    changes to edit songs
    pas op als je een song verwijdert wordt deze nu ook uit de server verwijdert hierdoor onstaan er geen problemen met vollopende servers met "junk" files die niet te bereiken zijn

--- include/admin_edit_songs.php
+++ include/admin_edit_songs.php
@@ -5,10 +5,31 @@ include 'include/function.php';
 //this deletes a record if the button is pushed
 if (!empty($_POST['admin_edit_songs_delete']))
 {
-$Delete_query="DELETE from songs where title ='".$_POST['admin_edit_songs_delete']."'";
 
-set_error_handler("custom_error_ErrorHandler_for_songs");
-mysqli_query($db, $Delete_query) or trigger_error('an in use error');
+//hier wordt de muziek file verwijdert
+$query_select="SELECT file_location from songs where title ='".$_POST['admin_edit_songs_delete']."'";
+//set_error_handler("custom_error_ErrorHandler_for_songs_location");
+$location = mysqli_query($db, $query_select) or die("FOUT : " . mysqli_error($db)); //trigger_error('an in use error');
+$location= mysqli_fetch_assoc($location);
+var_dump($location['file_location']);
+
+    if (file_exists($location['file_location'])) {
+        unlink($location['file_location']);
+        }
+          //dit verwijdert de cart_songs van de user
+  $Delete_query_cart_songs="DELETE from cart_songs where songs_title ='".$_POST['admin_edit_songs_delete']."'";
+  set_error_handler("custom_error_ErrorHandler_for_users_cart_songs");
+  mysqli_query($db, $Delete_query_cart_songs) or trigger_error('an in use error');
+
+  //dit verwijdert de library_songs van de user
+  $Delete_query_library_songs="DELETE from library_songs where songs_title ='".$_POST['admin_edit_songs_delete']."'";
+  set_error_handler("custom_error_ErrorHandler_for_users_library_songs");
+  mysqli_query($db, $Delete_query_library_songs) or trigger_error('an in use error');
+
+  //dit verwijdert de song
+  $Delete_query="DELETE from songs where title ='".$_POST['admin_edit_songs_delete']."'";
+  set_error_handler("custom_error_ErrorHandler_for_songs");
+  mysqli_query($db, $Delete_query) or trigger_error('an in use error');
 
 
 $_POST['profile']='tracklist';
--- include/function.php
+++ include/function.php
@@ -7,11 +7,17 @@ function custom_error_ErrorHandler_for_genre($errno, $errstr, $errfile, $errline
     echo '<div class="alert alert-danger" role="alert">this genre is still in use, please remove this genre from all records before changing or removing it.</div>';
 }
 
+
 function custom_error_ErrorHandler_for_songs($errno, $errstr, $errfile, $errline) {
-    echo '<div class="alert alert-danger" role="alert">this song is still in use, please remove this song from all  other records before changing or removing it.</div>';
+    echo '<div class="alert alert-danger" role="alert">something went wrong trying to delete the song record</div>';
 }
 
-//hier begint de  custom error handler van voor de account page
+
+function custom_error_ErrorHandler_for_songs_location($errno, $errstr, $errfile, $errline) {
+    echo '<div class="alert alert-danger" role="alert">something went wrong trying to get the songs location</div>';
+}
+
+
 function custom_error_ErrorHandler_for_users_cart_songs($errno, $errstr, $errfile, $errline) {
     echo '<div class="alert alert-danger" role="alert">something went wrong trying to delete the songs from the shoppingcart</div>';
 }

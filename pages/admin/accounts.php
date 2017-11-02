0aac3222b4d01a792e35d515cd24e40d6eeb0c66
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-11-02 14:35:09 +0100

    boo

--- pages/admin/accounts.php
+++ pages/admin/accounts.php
@@ -1,32 +1,38 @@
-<?php /* WIP
 <div class="col">
-    </br>
-       <div class="card">
-  <div class="card-body">
-     <form class="" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-                        <legend>Account Details</legend>
-                        <div class="row">
-      <div class="form-group col-md-4">
-      <label for="">Existing Password </label><input type="password" class="form-control" name="password" >
+  <h2 id="white" class="display-2">Users</h2>
+      <br>
+      <div class="card">
+        <div class="card-body">
+          <div class="container container-table">
+            <div class="table-wrapper">
+              <table class="table" cellspacing="0">
+                  <tr class="table-heads">
+                    <th class="head-item mbr-fonts-style display-7">
+                      Username
+                    </th>
+                    <th class="head-item mbr-fonts-style display-7">
+                      First Name
+                    </th>
+                    <th class="head-item mbr-fonts-style display-7">
+                      Last Name
+                    </th>
+                    <th class="head-item mbr-fonts-style display-7">
+                      Email
+                    </th>
+                    <th class="head-item mbr-fonts-style display-7">
+                      Admin
+                    </th>
+                  </tr>
+                <tr>
+                <td class='body-item mbr-fonts-style display-7'>{$row['title']}'</td>
+                <td class='body-item mbr-fonts-style display-7'>{$row['artiest']}</td>
+                <td class='body-item mbr-fonts-style display-7'>{$row['album_title']}</td>
+                <td class='body-item mbr-fonts-style display-7'>button</td>
+                <td class='body-item mbr-fonts-style display-7'>{$row['artiest']}</td>
+                </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="row">
-  <div class="form-group col-md-4">
-       <label for=""> password </label><input type="password" class="form-control" name="npassword" >
-      </div>
-    <div class="form-group col-md-4">
-       <label for="">re password </label><input type="password" class="form-control" name="repassword" >
-      </div>
-    </div>
- <div class="row">
- 	<div class="col">
-      <button class="btn btn-success" type="submit" name="password_change" value="password_change">Change</buttton>
-     	</div
- </div>
-      </form>
-    </div>
-</br>
-    <?php include 'include/password_script.php' ?>
-  </div>
-</div> 
-*/ ?>

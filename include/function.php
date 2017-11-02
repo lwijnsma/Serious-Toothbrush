440f841b227efbe306af8051f31f55ecbb5bfdb3
jeroenLinux <32095387+jeroenLinux@users.noreply.github.com>
2017-11-02 21:59:45 +0100

    dit werkt nu yay

--- include/function.php
+++ include/function.php
@@ -3,6 +3,9 @@ function custom_error_ErrorHandler_for_album($errno, $errstr, $errfile, $errline
     echo '<div class="alert alert-danger" role="alert">this album is still in use, please remove this album from all records before changing or removing it.</div>';
 }
 
+function custom_error_ErrorHandler_for_genre($errno, $errstr, $errfile, $errline) {
+    echo '<div class="alert alert-danger" role="alert">this genre is still in use, please remove this genre from all records before changing or removing it.</div>';
+}
 
 
  ?>

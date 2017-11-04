5343b4887c7fec907f2bd7ddaf990d0cd1b8d2e8
laurens wijnsma <wijnsmalaurens@gmail.com>
2017-11-04 19:12:10 +0100

    Update edit.php

--- pages/admin/edit.php
+++ pages/admin/edit.php
@@ -7,20 +7,20 @@
          <form class="" role="form" action="<?php ($_SERVER['PHP_SELF']);?>" method="post">
             <div class="row">
                <div class="col-md-4">
-                  <label for=""> Username </label><input type="text" class="form-control" name="user" value="<?php print $_SESSION["gerbruiker_informatie"]["username"]; ?>" size="20" disabled="true">
+                  <label for=""> Username </label><input type="text" class="form-control" name="user" value="<?php print $_SESSION["gebruiker_informatie"]["username"]; ?>" size="20" disabled="true">
                </div>
             </div>
             <div class="row">
                <div class="col-md-4">
-                  <label for=""> Voornaam </label><input type="text" class="form-control" name="profile_name" value="<?php  print $_SESSION["gerbruiker_informatie"]["first_name"]?>">
+                  <label for=""> Voornaam </label><input type="text" class="form-control" name="profile_name" value="<?php  print $_SESSION["gebruiker_informatie"]["first_name"]?>">
                </div>
                <div class="col-md-4">
-                  <label for="">Achternaam </label><input type="text" class="form-control" name="profile_lastname" value="<?php  print $_SESSION["gerbruiker_informatie"]["last_name"]?>">
+                  <label for="">Achternaam </label><input type="text" class="form-control" name="profile_lastname" value="<?php  print $_SESSION["gebruiker_informatie"]["last_name"]?>">
                </div>
             </div>
             <div class="row">
                <div class="form-group col-md-8">
-                  <label for="">Email </label><input type="text" class="form-control" name="profile_email" value="<?php  print $_SESSION["gerbruiker_informatie"]["email"]?>" size="20p">
+                  <label for="">Email </label><input type="text" class="form-control" name="profile_email" value="<?php  print $_SESSION["gebruiker_informatie"]["email"]?>" size="20p">
                </div>
             </div>
             <div class="row">
